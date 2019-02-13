9d8f9929be85a3e8fefb381cde7908c602b0d39f
Dimitry Rudakov <ext-dmytro.rudakov@here.com>
2019-02-13 09:26:37 +0200

    Insert and fetch data, gitignore updated

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,13 +1,15 @@
 //const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-var obj = new ObjectID();
+let obj = new ObjectID();
 
-console.log(obj);
+console.log('>>>>'+obj);
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
     if (err) {
-        return console.error('Unable to connect to MongoDb server');
+        return console.error('Unable to connect to MongoDB server');
     }
     console.log('Connected to MongoDB server');
 
@@ -15,23 +17,63 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     text: 'Something to do',
     //     completed: false
     // }, (err, result) => {
-    //     if(err){
-    //         return console.error('Unable to insert todos', err);
+    //
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
     //     }
     //
     //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // })
+    // });
 
     // db.collection('Users').insertOne({
     //     name: 'Dimitry',
     //     age: 29,
-    //     location: 'Kyiv, Ukraine'
+    //     location: 'Kyiv'
     // }, (err, result) => {
     //     if (err) {
+    //         return console.log('Unable to insert user', err);
     //     }
-    //         console.error('Unable to insert user to MongoDB');
+    //
     //     console.log(result.ops[0]._id.getTimestamp());
-    // });
+    // })
 
     db.close();
 });
+
+
+//
+// const {MongoClient, ObjectID} = require('mongodb');
+// var obj = new ObjectID();
+//
+// console.log(obj);
+//
+// MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+//     if (err) {
+//         return console.error('Unable to connect to MongoDb server');
+//     }
+//     console.log('Connected to MongoDB server');
+//
+//     // db.collection('Todos').insertOne({
+//     //     text: 'Something to do',
+//     //     completed: false
+//     // }, (err, result) => {
+//     //     if(err){
+//     //         return console.error('Unable to insert todos', err);
+//     //     }
+//     //
+//     //     console.log(JSON.stringify(result.ops, undefined, 2));
+//     // })
+//
+//     // db.collection('Users').insertOne({
+//     //     name: 'Dimitry',
+//     //     age: 29,
+//     //     location: 'Kyiv, Ukraine'
+//     // }, (err, result) => {
+//     //     if (err) {
+//     //     }
+//     //         console.error('Unable to insert user to MongoDB');
+//     //     console.log(result.ops[0]._id.getTimestamp());
+//     // });
+//
+//     db.close();
+// });
--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,9 +1,4 @@
-//const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
-
-var obj = new ObjectID();
-
-console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
@@ -11,16 +6,24 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB server');
 
-    // db.collection('Todos').find().count().then(count => {
-    //     console.log(`Todos count:${count}`);
-    // }, err =>{
-    //     console.log("Unable to fetch todos", err);
-    // });
+    // db.collection('Todos').find({
+    //     _id: new ObjectID("5c63c03f345175bba8157af4")
+    // }).toArray().then(docs => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    //
+    // }, err => {
+    //
+    //     console.log("Unable to fetch todo", err);
+    // })
 
-    db.collection('Users').find({name: 'Dimitry'}).toArray().then(docs =>{
-        console.log(JSON.stringify(docs, undefined, 2));
+    db.collection('Users').find({name: "Andrew"}).count().then(counts => {
+        console.log(`Todos count: ${counts}`);
+
+
     }, err => {
-        console.log('Unable fetch data', err);
+
+        console.log("Unable to fetch todo", err);
     })
 
     db.close();
