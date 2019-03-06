cf9d7eade0dad4aa98534d14b93f285b99916938
Dimitry Rudakov <ext-dmytro.rudakov@here.com>
2019-03-06 21:23:22 +0200

    Fix MONGO_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose')
-const User = require('../models/user.js')
+const mongoose = require('mongoose');
+const User = require('../models/user.js');
 
-mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/TodoApp', { useMongoClient: true })
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useMongoClient: true });
 
-module.exports = { mongoose }
+module.exports = { mongoose };
 
