e6e9e6d59c4f2467d1f0e41e24dea1ea15781aca
Dimitry Rudakov <ext-dmytro.rudakov@here.com>
2019-03-16 14:00:42 +0200

    Add DELETE route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const User = require('../models/user.js');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
 
--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
 
     let id = req.params.id
-    console.log(id)
+    console.log(req)
     if (!ObjectID.isValid(id)) {
         return res.status(400).send()
     }
@@ -49,6 +49,27 @@ app.get('/todos/:id', (req, res) => {
         console.log(err)
     })
 })
+
+app.delete('/todos/:id', (req, res) => {
+
+    let id = req.params.id
+    console.log(id)
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+
+    Todo.findByIdAndRemove(id).then(todo =>{
+        if(!todo){
+            return res.status(400).send()
+        }
+    res.send({todo});
+
+    })
+    .catch(e => {
+        console.log(e)
+        res.status(400).send()
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started up on the port ${port}`);
